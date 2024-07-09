f01c70f9b353813d2e7ac672e1287291e68f111c
Gregor Schafroth <gregor.schafroth@gmail.com>
2024-07-09 16:31:03 +0200

    Interfaces

--- dist/index.js
+++ dist/index.js
@@ -1,23 +1,2 @@
 "use strict";
-class Person {
-    constructor(firstName, lastName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
-    get fullName() {
-        return this.firstName + ' ' + this.lastName;
-    }
-}
-class Student extends Person {
-    takeTest() {
-        console.log('Taking a test');
-    }
-}
-class Teacher extends Person {
-    get fullName() {
-        return 'Professor ' + super.fullName;
-    }
-}
-let teacher = new Teacher('John', 'Smith');
-console.log(teacher.fullName);
 //# sourceMappingURL=index.js.map
